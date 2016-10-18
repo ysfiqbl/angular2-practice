3143db20a17ba76cba7ffe95e2e5def0f7b76cc7
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-18 13:44:10 +0530

    Generated employee class and unit test.

---
+++ src/app/employee.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Employee} from './employee';
+
+describe('Employee', () => {
+  it('should create an instance', () => {
+    expect(new Employee()).toBeTruthy();
+  });
+});
