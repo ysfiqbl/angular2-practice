bf1506cff5513d4cf2cf9aec1df583855ff38683
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-18 13:44:49 +0530

    Generated employee component and created bindings.

---
+++ src/app/employee/employee.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { EmployeeComponent } from './employee.component';
+
+describe('Component: Employee', () => {
+  it('should create an instance', () => {
+    let component = new EmployeeComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  employee: Employee;
+
+  constructor() { 
+    this.employee = new Employee();
+  }
+
+  ngOnInit() {
+  }
+
+  add() {
+    console.log("Logic to send object to server");
+    console.log(this.employee);
+  }
+
+}
