7da7382f9f603ce575a6a0101fb8de2d4f4beb9b
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-21 13:36:11 +0530

    Added comments and changed url.

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -10,17 +10,36 @@ export class EmployeeService {
   url: string;
 
   constructor(private http: Http) { 
-    this.url = 'http://localhost:7150/employees/'
+    this.url = 'http://localhost:7150/api/employees/'
   }
 
-  getEmployees(): Promise<any[]> {
+  /**
+   * Make HTTP GET request to get a list of all employees. 
+   */
+  all(): Promise<Employee[]> {
     return this.http.get(this.url).toPromise().then((response) => {
-      console.log(response);
-      return response.json();
+      return response.json() as Employee;
     });
   }
 
-  delete(id: number) {
+  /**
+   * Make HTTP POST request to create a new employee. The response contains
+   * the id of the newly created employee.
+   * 
+   * @param employee - employee object that needs to be created
+   */
+  create(employee: Employee): Promise<any> {
+    return this.http.post(this.url, employee).toPromise().then((response) => {
+      return response.json();
+    })
+  }
+
+  /**
+   * Make HTTP DELETE request to delete the an employee with id
+   * 
+   * @param id - the id of the employee to be deleted
+   */
+  delete(id: number): Promise<number> {
     return this.http.delete(`${this.url}${id}`).toPromise().then((response) => {
       return response.status;
     });
