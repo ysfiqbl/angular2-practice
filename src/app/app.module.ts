85fdbbf86a315b99097d244e667f6431b345badd
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-21 09:54:42 +0530

    Added EMployeeService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { DistributorComponent } from './distributor/distributor.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeService } from './employee/employee.service';
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { EmployeeListComponent } from './employee/employee-list/employee-list.co
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
